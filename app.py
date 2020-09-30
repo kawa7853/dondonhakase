f7649949d3bc39f9d32446e807ea2eb2eb8f60b3
Kawa Araki <qq86853130pp@gmail.com>
2020-09-30 11:31:31 +0800

    yan

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    s = "安安"
+    s = "やん？"
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text = s))
